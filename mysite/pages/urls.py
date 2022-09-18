29b1c57fce99e10ff4b45cbf75a7a10f81314660
Latosuo <mitro.latosuo@student.hamk.fi>
2022-09-18 21:46:48 +0300

    Login ja logout rakenneltu

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
 app_name = 'pages'
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
