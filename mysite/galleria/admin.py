91b92336bb5b5e2d5af5c3e73d0ef879c8c29f9f
elisarissanen <elisa.rissanen1@gmail.com>
2022-09-19 19:20:28 +0300

    galleria admin.py lisätty, versio: kökkö

--- mysite/galleria/admin.py
+++ mysite/galleria/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, UserGallery, UserPhoto
+
+# Tää on nyt sit erittäin kökkö ja ei toimi niin kun haluun
+
+#class UserAdmin(admin.ModelAdmin):
+    #list_display = ('name', 'created')
+    #list_filter = ['name']
+    #search_fields = ['name']
+
+admin.site.register(User)
+#admin.site.register(UserAdmin)
+admin.site.register(UserGallery)
+admin.site.register(UserPhoto)
--- mysite/galleria/models.py
+++ mysite/galleria/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    """Kommentti"""
+    """Database user information"""
     
-    # Django luo itse user id:n, alkaa 1
-    # User.objects.get(id=1)
+    # Django luo itse user id:n, alkaa 1, User.objects.get(id=1)
+    # Käytetään tätä käyttäjän referointiin?
+
+    #created = models.DateTimeField('date created', auto_now_add=True)
+    name = models.CharField(max_length=50, default="user")
 
     def __str__(self):
         return self.id
@@ -18,16 +21,13 @@ class UserGallery(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
-    #def __str__(self):
-        #return ??
-
-
 class UserPhoto(models.Model):
-    """Kommentti"""
+    """Single photo with a name, added time, and the photo"""
     usergallery = models.ForeignKey(UserGallery, on_delete=models.CASCADE)
     
-    name = models.CharField(max_length=50, default=None) # photo name?
-    img = models.BinaryField() # save photo as binary object
+    name = models.CharField(max_length=50, default=None) # photo name? joku generointi tähän?
+    img = models.BinaryField(default=None) # save photo as binary object, tähän tarvitsisi lisätä joku default kuva? miten toimii binääri?
+    #created = models.DateTimeField('date created', auto_now_add=True)
     
     # TAGS, erillinen lista?
 
