a47169c06213cb1d05fb6df4d475f1f37d6f395c
Latosuo <mitro.latosuo@student.hamk.fi>
2022-09-26 19:21:14 +0300

    korjattu vielä uploadin pikkuvika

--- mysite/galleria/models.py
+++ mysite/galleria/models.py
@@ -19,13 +19,13 @@ class UserGallery(models.Model):
     """Database object UserGallery to store images"""
     user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
 
-def user_directory_path(instance, filename):
+#def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+#    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class UserPhoto(models.Model):
     """ kuvan tallennus käyttäjäID:tä vastaavaan sijaintiin media kansiossa """
-    image = models.ImageField(upload_to=user_directory_path)
+    image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     mod_date = models.DateTimeField('date modified', auto_now=True)
