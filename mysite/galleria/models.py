9446a617a887da0a46d255da24e0ddf012e8ddee
elisarissanen <elisa.rissanen1@gmail.com>
2022-10-11 07:59:18 +0300

    Formatointia, roskan poistamista, yri suoralinkitykseen, purkkaratkaisu saattaa toimia?

--- mysite/galleria/models.py
+++ mysite/galleria/models.py
@@ -9,60 +9,48 @@ from imagekit.processors import ResizeToFill
 from taggit.managers import TaggableManager
 
 
-# Create your models here.
-
-# TÄTÄ USER MODELIA EI VÄLTTIS KÄYTETÄ
-#class User(models.Model):
-#    """Database user information"""
-#
-    # Django luo itse user id:n, alkaa 1, User.objects.get(id=1)
-    # Käytetään tätä käyttäjän referointiin?
-
-#    date = models.DateTimeField('date created', auto_now_add=True)
-#    name = models.CharField(max_length=50, default="user") # tää default on aika sos mut testaukseen
-
-   # def __str__(self):
-   #     return self.name
-
 def gallery_owner(instance):
     current_user = request.user
     return '{}'.format(instance.current_user)
 
+
 class UserGallery(models.Model):
     """Database object UserGallery to store images"""
-    #user = models.OneToOneField(User,primary_key=True,on_delete=models.CASCADE,#help_text=_('User (Required).'),)
     id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(User, default=User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User,
+                              default=User,
+                              on_delete=models.SET_NULL,
+                              null=True)
     name = models.CharField(max_length=20)
-    private = models.BooleanField(default = False)
+    private = models.BooleanField(default=False)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     mod_date = models.DateTimeField('date modified', auto_now=True)
 
-    #owner = gallery_owner(self)
-    
     def __str__(self):
         return self.name
 
 
-#Tällä autetaan tallennuskansio UserPhotoa ladatessa oikeaan gallerian kansioon
+# Tallennuskansio UserPhotoa ladatessa oikeaan gallerian kansioon
 def user_directory_path(instance, filename):
-    #file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    # File will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return 'gallery_{0}/{1}'.format(instance.galleria.id, filename)
-
 
 
 class UserPhoto(models.Model):
     """ kuvan tallennus käyttäjäID:tä vastaavaan sijaintiin media kansiossa """
-    #Tähän viittaus tiettyyn galleriaan esim:
-    #id = models.AutoField(primary_key=True)
-    
-    galleria = models.ForeignKey(UserGallery, on_delete=models.CASCADE, default = 0)
-    owner = models.ForeignKey(User, default=User, on_delete=models.SET_NULL, null=True)
+
+    galleria = models.ForeignKey(UserGallery,
+                                 on_delete=models.CASCADE,
+                                 default=0)
+    owner = models.ForeignKey(User,
+                              default=User,
+                              on_delete=models.SET_NULL,
+                              null=True)
     image = models.ImageField(upload_to=user_directory_path)
     image_preview = ImageSpecField(source='image',
-                                  processors=[ResizeToFill(100, 100)],
-                                  format='JPEG',
-                                  options={'quality': 60})
+                                   processors=[ResizeToFill(100, 100)],
+                                   format='JPEG',
+                                   options={'quality': 60})
 
     description = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
@@ -72,14 +60,3 @@ class UserPhoto(models.Model):
     @property
     def phohtos(self):
         return self.image_preview.all()
-
-   # usergallery = models.ForeignKey(UserGallery, on_delete=models.CASCADE,blank=True,null=True)
-    
-    #name = models.CharField(max_length=50, default=None) # photo name? joku generointi tähän?
-    #img = models.BinaryField(default=None) # save photo as binary object, tähän tarvitsisi lisätä joku default kuva? miten toimii binääri?
-    #created = models.DateTimeField('date created', auto_now_add=True)
-    
-    # TAGS, erillinen lista?
-
-#    def __str__(self):
-#       return self.name
--- mysite/galleria/urls.py
+++ mysite/galleria/urls.py
@@ -10,16 +10,16 @@ from .views import *
 from galleria.views import display_images
 from . import views
 
-
 app_name = 'galleria'
 urlpatterns = [
-    path('<int:id>/', display_images, name = 'display_images'),
-    path('tags/<str:tag>', ITdisplay_images, name = 'display_images'), # iteroitava displöö images
-    path('', display_galleries, name = 'display_galleries'),
-    path('image_upload', image_upload, name = 'image_upload'),
-    path('create_gallery', create_gallery, name = 'create_gallery'),
-    path('success', success, name = 'success'),
-    path('successgal', successgal, name = 'successgal'),
+    path('<int:id>/', display_images, name='display_images'),
+    path('tags/<str:tag>', ITdisplay_images,
+         name='display_images'),  # iteroitava displöö images
+    path('', display_galleries, name='display_galleries'),
+    path('image_upload', image_upload, name='image_upload'),
+    path('create_gallery', create_gallery, name='create_gallery'),
+    path('success', success, name='success'),
+    path('successgal', successgal, name='successgal'),
     path('tags', tags, name='tags'),
     path('delete/<int:id>', views.deleteimage, name='deleteimage'),
     path('deletegallery/<int:id>', views.deletegallery, name='deletegallery'),
